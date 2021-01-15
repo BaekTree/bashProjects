bf1bde3cb9bd1763630974d34a0ffc0ca1c1f41f
baekTree <etknbe@gmail.com>
2021-01-16 01:19:34 +0900

    [update] link the limit number in rules.txt to the real limit of each questions. [fix] fix bug, which required limit number of answer in a question with free answer. It was the problem caused from reading with nameref array. [update] using nameref to read and save rules.

--- codeRepentGradient/function_up.sh
+++ codeRepentGradient/function_up.sh
@@ -120,6 +120,41 @@ record(){
     echo -e $1 >> preq.txt
 }
 
+printArrs(){
+    #print Arr
+    echo -e "\n\n\nprint all items"
+    for (( i=0; i<${#ruleArr[@]}; i++ ))
+    do
+        
+        echo -e "ruleArr${i} : ${ruleArr[$i]}"
+        echo
+        echo -e "contArr${i} : ${contArr[$i]}"
+        echo
+        echo -e "ansArr${i} : ${ansArr[$i]}"
+        echo
+        echo -e "limitArr${i} : ${limitArr[$i]}"
+        echo -e "-----\n\n\n"
+        # for (( j=0; j<${#RAC[@]}; j++ ))
+        # do
+            
+        #     # echo ${RAC[$j]}
+        #     name=${RAC[$j]}
+        #     # echo $name
+        #     name=${name}Arr
+        #     echo ${name}${i}
+        #     echo ${!name[$i]}
+        #     # echo
+        #     # echo ${${!${RAC[$j]}Arr}[$i]}
+        # done
+    done
+    echo size of ruleArr : ${#ruleArr[@]}
+    echo size of contArr : ${#contArr[@]}
+    echo size of ansArr : ${#ansArr[@]}
+    echo size of limitArr : ${#limitArr[@]}
+
+    echo
+}
+
 << "READ"
 
 테스트케이스
@@ -132,18 +167,290 @@ cont나 ans을 만나면 그때 contArr, ansArr에 넣는다.
 현재 저장할 arr을 명시해둔다. 근데 그게 되나? bash에서?
 
 curArr=$ruleArr
-curAA+=
+curAA+=...
 
-READ
+---
+
+전통적인 시도
+테스트케이스
+rule을 만난다. 
+빈칸이나 cont가 있거나 ans가 있지 않으면 모두 ruleArr[i]에 저장한다.
+ruleArr+='\n'nextLine
+
+만약 빈줄을 만났는데, 현재 arr이 rule이었다면
+cont, ans, limit arr을 모두 저장해야 한다. 
+
+if curArr=rule
+    fill cont
+    fill ans
+    fill limit
+if curArr=cont
+    fill ans
+    fill limit
+
+만약 array을 넣거나 nameref을 넣을 수 있다면?
+---
 
 
+i=0
+order=0
+while read line
+    line을 읽는다
+    if line = empty line
+        for idx = order to len RAC
+            tempArr=RAC[idx] arr
+            tempArr+=" "
+        order=0
+        continue
+
+    is rule, cont, ans, limit?
+    if rule
+        i=0
+        curArr[rule]
+        curArr+=(line)
+    if cont
+        i=1
+        curArr[cont]
+        curArr+=(line)
+    if ans
+        i=2
+        curArr[ans]
+        curArr+=(line)
+    if limit
+        i=3
+        curArr[limit]
+        curArr+=(line)
+    
+
+    if order < i
+        for idx = order to i
+            tempArr=RAC[idx] arr
+            tempArr+=" "
+        order = i
+
+    order++
+
+    if order > 2
+        order = -1
+    
+
+
+done
+
+---
 
 declare -a ruleArr
 declare -a contArr
 declare -a ansArr
 declare -a limitArr
 
+allArr=( ruleArr contArr ansArr limitArr )
+arrIdx=0
+while read line
+do
+    if [ line = "" ]
+        local read_i
+        for (( read_i=$arrIdx; read_i<${#allArr[@]}; read_i++ ))
+        do
+            t=${allArr[$read_i]}
+            declare -n tmpArr=${t[@]}
+            tempArr+=(" ")
+        done
+        arrIdx=0
+        continue
+
+    t=${allArr[$arrIdx]}
+    declare -n tmpArr=${t[@]}
+    tempArr+=(line)
+    arrIdx=$(( arrIdx + 1 ))
+    if [ arrIdx \gt 2 ]
+    then
+        arrIdx=0
+    fi
+done
+
+
+---
+
+A : ( rule cont ans limit )
+for i in A
+if curArr[i] = rule
+    idx=i
+    break
+
+for i=idx to A.len
+    fill A[i]
+
+---
+검색
+https://unix.stackexchange.com/questions/545502/bash-array-of-arrays
+
+
+declare -a ruleArr
+declare -a contArr
+declare -a ansArr
+declare -a limitArr
+arr=( ruleArr contArr ansArr limitArr )
+
+for i in $arr
+do
+    declare -n temtArr=$i
+
+    for j in $tempArr
+        j+=(...)
+done
+
+이것을 적용하면...
+
+while read line
+do
+    if line = empty line
+        declare -n arr=
+done
+
+
+
+
+
+READ
+
+
+# fillArr(){
+#     declare -n arr=$1
+#     arr+=($2)
+# }
+declare -a ruleArr=()
+declare -a contArr=()
+declare -a ansArr=()
+declare -a limitArr=()
+
 getRules(){
+    file='./rules.txt'
+    allArr=( ruleArr contArr ansArr limitArr )
+
+    order=${#allArr[@]}
+    arrIdx=0
+
+    ruleNum=1
+
+    while read line;
+    do  
+        # echo
+        # echo
+        # echo "line${line}line"
+        # echo
+        # echo
+        if [[ $line == "" ]]
+        then
+            echo newline
+            continue
+        fi
+
+        if [[ $line == *"rule"* ]]
+        then
+            echo
+            arrIdx=0
+            r="${line#"rule : "}"
+            # echo "get rule $r"
+            # tmpArr+=("$r")
+            rules+="RULE ${ruleNum} : $r\n\n\n\n"
+            ruleNum=$(( ruleNum + 1 ))
+            
+            t=${allArr[$arrIdx]}
+            declare -n tmpArr=${t[@]}
+            tmpArr+=("$r")
+            echo "t=$t, r=$r"
+            echo ${tmpArr[@]}
+            
+            # 새로운 빈줄을 만났을 때 남아있는 cont ans limit을 초기화한다.
+            local read_i
+            # echo ----------
+
+            echo "new : from $order to ${#allArr[@]}"
+            for (( read_i=$order; read_i<${#allArr[@]}; read_i++ ))
+            do
+                t=${allArr[$read_i]}
+                declare -n tmpArr=${t[@]}
+                tmpArr+=(" ")
+                echo "t=$t, r=\" \""
+                echo ${tmpArr[@]}
+            done
+            order=1
+
+            continue
+        fi
+        
+        
+
+
+        if [[ $line == *"cont"* ]]
+        then
+            # echo "get cont $line"
+            arrIdx=1
+            r=("${line#"cont : "}")
+            # echo $r
+        elif [[ $line == *"ans"* ]]
+        then
+            # echo "get ans $line"
+            arrIdx=2
+            r=("${line#"ans : "}")
+            # echo $r
+        elif [[ $line == *"limit"* ]]
+        then
+            # echo "Get limit $line"
+            arrIdx=3
+            r=("${line#"limit : "}")
+            # echo $r
+        fi
+
+        # echo order=$order
+        t=${allArr[$arrIdx]}
+        declare -n tmpArr=${t[@]}
+        tmpArr+=("$r")
+        echo "t=$t, r=$r"
+        echo ${tmpArr[@]}
+
+
+        # echo tmpArr=$tmpArr
+        # echo line=$line
+        # tmpArr+=("$line")
+
+
+        # echo "----------order : $order and arrIdx : $arrIdx-----------"
+        if [ $order -lt $arrIdx ]
+        then
+            # echo "runeNum=$ruleNum"
+            # echo ruleArr
+            echo "skip : from $order to $arrIdx"
+
+            local read_i
+            for (( read_i=$order; read_i<$arrIdx; read_i++ ))
+            do
+                t=${allArr[$read_i]}
+                # echo "target array : $t = \" \""
+                declare -n tmpArr=${t[@]}
+                # echo "temp arr : $tmpArr"
+                tmpArr+=(" ")
+            done
+            order=$arrIdx
+        fi
+
+        order=$(( order + 1 ))
+
+        # if [ $order -gt 3 ]
+        # then
+        #     order=0
+        # fi
+    done < $file
+
+    printArrs
+    # echo $rules
+    
+
+}
+
+
+getRules2(){
     file='./rules.txt'
     idx=1
     while read line;
@@ -180,33 +487,10 @@ getRules(){
 
     done < $file
 
-    #print Arr
-    echo -e "\n\n\nprint all items"
-    for (( i=0; i<${#ruleArr[@]}; i++ ))
-    do
-        
-        echo "rule${i} : ${ruleArr[$i]}"
-        echo
-        echo "cont${i} : ${contArr[$i]}"
-        echo
-        echo "ans${i} : ${ansArr[$i]}"
-        # for (( j=0; j<${#RAC[@]}; j++ ))
-        # do
-            
-        #     # echo ${RAC[$j]}
-        #     name=${RAC[$j]}
-        #     # echo $name
-        #     name=${name}Arr
-        #     echo ${name}${i}
-        #     echo ${!name[$i]}
-        #     # echo
-        #     # echo ${${!${RAC[$j]}Arr}[$i]}
-        # done
-        echo -----
-        echo
-        echo
 
-    done
+
+    
+
 }
 
 
@@ -414,13 +698,19 @@ REPEAT
         
         correctRes="${ansArr[$i]}"
         echo "correct response : $correctRes"
+        resStr="PLEASE TYPE <$correctRes>\n\nDO NOT FORGET CPAS LOCK.\nDO NOT FORGET PERIOD."
 
-
-        apple_text "${ruleArr[${i}]}\n\n\n\n${contArr[${i}]}"
+        apple_text "${ruleArr[${i}]}\n\n\n\n${contArr[${i}]}\n\n\n$resStr"
         echo $ans
         echo $msg
 
-        limit=10
+        if [ ${limitArr[$i]} = " " ]
+        then
+            limit=0
+        else
+            limit=${limitArr[$i]}
+        fi
+
 
 << "PSEUDO"
  while (ans[i] != " " -a msg != ans[i]) -o (ans[i] = " " -a limit > #msg)
@@ -439,9 +729,27 @@ REPEAT
 msg^^
 
 PSEUDO
+
+    # cond1="$correctRes" != " " -a "${msg^^}" != "$correctRes"
+    # cond2="$correctRes" = " " -a ${#msg} -lt ${limit}
+
     while [ "$ans" = "No" -o \( "$correctRes" != " " -a "${msg^^}" != "$correctRes" \) -o \( "$correctRes" = " " -a ${#msg} -lt ${limit} \) ]
         # while [ "$ans" = "No" -o ${#msg} -lt ${limit} -o "$msg" != "${ansArr[$i]}" ]
         do
+            if [ "$correctRes" != " " -a "${msg^^}" != "$correctRes" ]
+            then
+                echo "글자가 틀렸다."
+                echo ${msg^^}
+                echo $correctRes
+            #     echo "false : $cond1"
+            # fi
+            elif [ "$correctRes" = " " -a ${#msg} -lt ${limit} ]
+            then
+                echo "글자 수 미달"
+                echo ${#msg}
+                echo ${limit}
+            #     echo "false : $cond2"
+            fi
             alertUpdate $msg $limit
             apple_text ${alrt}${warn}${ruleArr[${i}]}
         done
@@ -502,7 +810,7 @@ wait(){
         done
 
         limitT=$(( $fewHours * 3600 ))
-        echo "checkout limitT=$limitT"
+        # echo "checkout limitT=$limitT"
     fi
 
     #MINUTES
@@ -510,7 +818,7 @@ wait(){
 
     for (( i=0; i<${#arr[@]}; i++ ))
     do
-        echo i=$i
+        # echo i=$i
         if [ $limitT -gt $(( ${arr[$i]} * 60 )) ]
         then
             leftOver=$(( $limitT - $(( ${arr[$i]} * 60 )) ))
@@ -518,11 +826,11 @@ wait(){
             sleep $leftOver
             echo ${arr[$i]} mins left.
             limitT=$(( $limitT - $leftOver ))
-            echo "update limitT : $limitT"
+            # echo "update limitT : $limitT"
         fi
     done  
 
-    echo "checkout limitT=$limitT"
+    # echo "checkout limitT=$limitT"
 
 
     #SECONDS
